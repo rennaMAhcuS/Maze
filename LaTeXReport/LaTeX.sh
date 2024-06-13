41b80a69cb528f9d04f5cc4092397dc5cc1c82c9
rennaMAhcuS <rennaMAhcuS@users.noreply.github.com>
2024-06-13 09:21:42 +0530

    Minor Changes

--- LaTeXReport/LaTeX.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-#pdflatex report > /dev/null
-#bibtex report > /dev/null
-#pdflatex report > /dev/null
-#pdflatex report > /dev/null
-open report.pdf
-cp report.pdf /Users/emsr/Documents/bodhitreeWorkspace/23b1017_iitb_ac_in/6/145/582/labDirectory/
